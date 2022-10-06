bc5b70c40bf58c578d7935cc1ad63ced15c67130
Max <omelchukmaxim94@gmail.com>
2022-10-06 17:25:02 +0100

    add first time

---
+++ main.js
@@ -0,0 +1,42 @@
+// $(document).ready(function () {
+//   $(".slider").slick();
+// });
+$(".slider").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+$(document).ready(function () {
+  var mouseX, mouseY;
+  var ww = $(window).width();
+  var wh = $(window).height();
+  var traX, traY;
+  $(document).mousemove(function (e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    traX = (4 * mouseX) / 570 + 40;
+    traY = (4 * mouseY) / 570 + 50;
+    console.log(traX);
+    $(".title").css({ "background-position": traX + "%" + traY + "%" });
+  });
+});
